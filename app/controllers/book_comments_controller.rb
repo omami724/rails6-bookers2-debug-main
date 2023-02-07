d8584baa4c9c788b367d94a71e341c7cda205b49
MamiKido <o73724@gmail.com>
2023-02-07 09:02:07 +0000

    nofinish

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,11 @@
+class BookCommentsController < ApplicationController
+ 
+ def create
+   redirect_to _path
+  end
+
+  def destroy
+   redirect_to _path
+  end
+  
+end
---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,11 @@
+class FavoritesController < ApplicationController
+  def create
+   redirect_to _path
+  end
+
+  def destroy
+   redirect_to _path
+  end
+
+end
+
